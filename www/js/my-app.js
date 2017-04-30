dbd421d389dbee2f85909392e4dc827658ca7bff
Leonardo Lira Becerra <leonardolirabecerra@MacBook-Pro-de-Leonardo.local>
2017-04-30 04:18:47 -0500

    Inicio

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -38,6 +38,10 @@ $$(document).on('pageInit', function (e) {
         // Following code will be executed for page with data-page attribute equal to "about"
         myApp.alert('Here comes About page');
     }
+
+    if (page.name == 'login') {
+        myApp.params.swipePanel = false;
+    }
 });
 
 // Option 2. Using live 'pageInit' event handlers for each page
